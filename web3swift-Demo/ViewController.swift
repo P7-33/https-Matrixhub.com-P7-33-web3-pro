12bf6816e061779596fe67da0394f5048dbbe2c0
Petr Korolev <sky4winder@gmail.com>
2020-02-20 12:49:51 +0700

    add target

---
+++ web3swift-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  web3swift-Demo
+//
+//  Created by Petr Korolev on 20.02.2020.
+//  Copyright © 2020 Matter Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
